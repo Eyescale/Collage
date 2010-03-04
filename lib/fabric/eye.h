89fd760229b0340f4c239279a2db9368a6d16b26
Stefan Eilemann <eilemann@gmail.com>
2010-03-04 10:25:13 +0000

    Move RenderContext and friends into fabric namespace
    [x] May break build
    [ ] May break existing applications
    [ ] Bugfix
    [ ] New Feature
    [x] Cleanup
    [ ] Optimization
    [ ] Documentation

---
+++ lib/fabric/eye.h
@@ -0,0 +1,42 @@
+
+/* Copyright (c) 2007-2009, Stefan Eilemann <eile@equalizergraphics.com> 
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 2.1 as published
+ * by the Free Software Foundation.
+ *  
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef EQFABRIC_EYE_H
+#define EQFABRIC_EYE_H
+
+#include <eq/base/base.h>
+#include <iostream>
+
+namespace eq
+{
+namespace fabric
+{
+    /**
+     * Defines an eye pass.
+     */
+    enum Eye
+    {
+        EYE_CYCLOP = 0,
+        EYE_LEFT,
+        EYE_RIGHT,
+        EYE_ALL   // must be last
+    };
+
+    EQ_EXPORT std::ostream& operator << ( std::ostream& os, const Eye& eye );
+}
+}
+#endif // EQFABRIC_EYE_H
---
+++ lib/fabric/range.h
@@ -0,0 +1,80 @@
+
+/* Copyright (c) 2006-2010, Stefan Eilemann <eile@equalizergraphics.com> 
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 2.1 as published
+ * by the Free Software Foundation.
+ *  
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef EQFABRIC_RANGE_H
+#define EQFABRIC_RANGE_H
+
+#include <eq/base/base.h>
+
+#include <iostream>
+
+namespace eq
+{
+namespace fabric
+{
+    /** Holds a fractional range along with some methods for manipulation. */
+    class Range 
+    {
+    public:
+        /**
+         * @name Constructors
+         */
+        //@{
+        Range() : start(0.f), end(1.f)  {}
+
+        Range( const float start_, const float end_ )
+                : start(start_), end(end_) {}
+        //@}
+
+        void apply( const Range& rhs )
+            {
+                const float w = end-start;
+                end    = start + rhs.end * w;
+                start += rhs.start * w;
+            }
+        
+        bool operator == ( const Range& rhs ) const
+        {
+            return start==rhs.start && end==rhs.end;
+        }
+        
+        bool operator != ( const Range& rhs ) const
+        {
+            return start!=rhs.start || end!=rhs.end;
+        }
+        
+        void invalidate() { start=0.f; end=0.f; }
+
+        bool isValid() const 
+            { return ( start>=0.f && end <=1.f && (end - start) >= 0.f ); }
+
+        bool hasData() const { return  (end - start) > 0.f; }
+
+        float start;
+        float end;
+
+        EQ_EXPORT static const Range ALL;
+    };
+
+    inline std::ostream& operator << ( std::ostream& os, const Range& range )
+    {
+        os << "range    [ " << range.start << " " << range.end << " ]";
+        return os;
+    }
+}
+}
+#endif // EQFABRIC_RANGE_H
