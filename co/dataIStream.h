a122e34b395871ccda56bdafdd24bffede7e6c42
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2012-09-14 09:54:28 +0200

    Add serialization support for map, hash_map, set, hash_set

--- co/dataIStream.h
+++ co/dataIStream.h
@@ -1,6 +1,7 @@
 
 /* Copyright (c) 2007-2012, Stefan Eilemann <eile@equalizergraphics.com>
  *               2009-2010, Cedric Stalder <cedric.stalder@gmail.com>
+ *                    2012, Daniel Nachbaur <danielnachbaur@gmail.com>
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License version 2.1 as published
@@ -23,6 +24,10 @@
 #include <co/array.h> // used inline
 #include <co/types.h>
 
+#include <lunchbox/stdExt.h>
+
+#include <map>
+#include <set>
 #include <vector>
 
 namespace co
@@ -58,15 +63,29 @@ namespace detail { class DataIStream; }
                 return *this;
             }
 
-        /** Byte-swap a plain data item. @version 1.0 */
-        template< class T > static void swap( T& value )
-            { lunchbox::byteswap( value ); }
-
         /** Read a lunchbox::Buffer. @version 1.0 */
         template< class T > DataIStream& operator >> ( lunchbox::Buffer< T >& );
 
         /** Read a std::vector of serializable items. @version 1.0 */
         template< class T > DataIStream& operator >> ( std::vector< T >& );
+
+        /** Read a std::map of serializable items. @version 1.0 */
+        template< class K, class V >
+        DataIStream& operator >> ( std::map< K, V >& );
+
+        /** Read a std::set of serializable items. @version 1.0 */
+        template< class T > DataIStream& operator >> ( std::set< T >& );
+
+        /** Read a stde::hash_map of serializable items. @version 1.0 */
+        template< class K, class V >
+        DataIStream& operator >> ( stde::hash_map< K, V >& );
+
+        /** Read a stde::hash_set of serializable items. @version 1.0 */
+        template< class T > DataIStream& operator >> ( stde::hash_set< T >& );
+
+        /** Byte-swap a plain data item. @version 1.0 */
+        template< class T > static void swap( T& value )
+            { lunchbox::byteswap( value ); }
 
         /** @internal
          * Deserialize child objects.
@@ -161,7 +180,7 @@ namespace detail { class DataIStream; }
         }
 
         /** Byte-swap a plain data item. @version 1.0 */
-        template< class T > void _swap( T& value ) const 
+        template< class T > void _swap( T& value ) const
             { if( isSwapping( )) swap( value ); }
 
         /** Byte-swap a C array. @version 1.0 */
@@ -176,193 +195,6 @@ namespace detail { class DataIStream; }
     };
 }
 
-#include <co/object.h>
-#include <co/objectVersion.h>
-
-namespace co
-{
-    /** @name Specialized input operators */
-    //@{
-    /** Read a std::string. */
-    template<>
-    inline DataIStream& DataIStream::operator >> ( std::string& str )
-    {
-        uint64_t nElems = 0;
-        *this >> nElems;
-        LBASSERTINFO( nElems <= getRemainingBufferSize(),
-                      nElems << " > " << getRemainingBufferSize( ));
-        if( nElems == 0 )
-            str.clear();
-        else
-            str.assign( static_cast< const char* >( getRemainingBuffer(nElems)),
-                        size_t( nElems ));
-        return *this;
-    }
-
-    /** Deserialize an object (id+version). */
-    template<> inline DataIStream& DataIStream::operator >> ( Object*& object )
-    {
-        ObjectVersion data;
-        *this >> data;
-        LBASSERT( object->getID() == data.identifier );
-        object->sync( data.version );
-        return *this;
-    }
-
-/** @cond IGNORE */
-    template< class T > inline DataIStream&
-    DataIStream::operator >> ( lunchbox::Buffer< T >& buffer )
-    {
-        uint64_t nElems = 0;
-        *this >> nElems;
-        buffer.resize( nElems );
-        return *this >> Array< T >( buffer.getData(), nElems );
-    }
-
-
-    template< class T > inline DataIStream&
-    DataIStream::operator >> ( std::vector< T >& value )
-    {
-        uint64_t nElems = 0;
-        *this >> nElems;
-        value.resize( nElems );
-        for( uint64_t i = 0; i < nElems; i++ )
-            *this >> value[i];
-
-        return *this;
-    }
-
-    namespace
-    {
-    class ObjectFinder
-    {
-    public:
-        ObjectFinder( const UUID& id ) : _id( id ) {}
-        bool operator()( co::Object* candidate )
-            { return candidate->getID() == _id; }
-
-    private:
-        const UUID _id;
-    };
-    }
-
-    template<> inline void DataIStream::_swap( Array< void > ) const { /*NOP*/ }
-
-    template< typename O, typename C > inline void
-    DataIStream::deserializeChildren( O* object, const std::vector< C* >& old_,
-                                      std::vector< C* >& result )
-    {
-        ObjectVersions versions;
-        *this >> versions;
-        std::vector< C* > old = old_;
-
-        // rebuild vector from serialized list
-        result.clear();
-        for( ObjectVersions::const_iterator i = versions.begin();
-             i != versions.end(); ++i )
-        {
-            const ObjectVersion& version = *i;
-
-            if( version.identifier == UUID::ZERO )
-            {
-                result.push_back( 0 );
-                continue;
-            }
-
-            typename std::vector< C* >::iterator j =
-                stde::find_if( old, ObjectFinder( version.identifier ));
-
-            if( j == old.end( )) // previously unknown child
-            {
-                C* child = 0;
-                object->create( &child );
-                LocalNodePtr localNode = object->getLocalNode();
-                LBASSERT( child );
-                LBASSERT( !object->isMaster( ));
-
-                LBCHECK( localNode->mapObject( child, version ));
-                result.push_back( child );
-            }
-            else
-            {
-                C* child = *j;
-                old.erase( j );
-                if( object->isMaster( ))
-                    child->sync( VERSION_HEAD );
-                else
-                    child->sync( version.version );
-
-                result.push_back( child );
-            }
-        }
-
-        while( !old.empty( )) // removed children
-        {
-            C* child = old.back();
-            old.pop_back();
-            if( !child )
-                continue;
-
-            if( child->isAttached() && !child->isMaster( ))
-            {
-                LocalNodePtr localNode = object->getLocalNode();
-                localNode->unmapObject( child );
-            }
-            object->release( child );
-        }
-    }
-/** @endcond */
-
-    /** Optimized specialization to read a std::vector of uint8_t. */
-    template<> inline DataIStream&
-    DataIStream::operator >> ( std::vector< uint8_t >& value )
-    { return _readFlatVector( value );}
-
-    /** Optimized specialization to read a std::vector of uint16_t. */
-    template<> inline DataIStream&
-    DataIStream::operator >> ( std::vector< uint16_t >& value )
-    { return _readFlatVector( value ); }
-
-    /** Optimized specialization to read a std::vector of int16_t. */
-    template<> inline DataIStream&
-    DataIStream::operator >> ( std::vector< int16_t >& value )
-    { return _readFlatVector( value ); }
-
-    /** Optimized specialization to read a std::vector of uint32_t. */
-    template<> inline DataIStream&
-    DataIStream::operator >> ( std::vector< uint32_t >& value )
-    { return _readFlatVector( value ); }
-
-    /** Optimized specialization to read a std::vector of int32_t. */
-    template<> inline DataIStream&
-    DataIStream::operator >> ( std::vector< int32_t >& value )
-    { return _readFlatVector( value ); }
-
-    /** Optimized specialization to read a std::vector of uint64_t. */
-    template<> inline DataIStream&
-    DataIStream::operator >> ( std::vector< uint64_t>& value )
-    { return _readFlatVector( value ); }
-
-    /** Optimized specialization to read a std::vector of int64_t. */
-    template<> inline DataIStream&
-    DataIStream::operator >> ( std::vector< int64_t >& value )
-    { return _readFlatVector( value ); }
-
-    /** Optimized specialization to read a std::vector of float. */
-    template<> inline DataIStream&
-    DataIStream::operator >> ( std::vector< float >& value )
-    { return _readFlatVector( value ); }
-
-    /** Optimized specialization to read a std::vector of double. */
-    template<> inline DataIStream&
-    DataIStream::operator >> ( std::vector< double >& value )
-    { return _readFlatVector( value ); }
-
-    /** Optimized specialization to read a std::vector of ObjectVersion. */
-    template<> inline DataIStream&
-    DataIStream::operator >> ( std::vector< ObjectVersion >& value )
-    { return _readFlatVector( value ); }
-    //@}
-}
+#include "dataIStream.ipp" // template implementation
 
 #endif //CO_DATAISTREAM_H
