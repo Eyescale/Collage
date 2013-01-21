Eyescale/Collage

commit e8aa2c7309fd93d85955ed68023c8fd416b69c49

Assert when adding connection descriptions to a live node

co/localNode.cpp
@@ -414,8 +414,10 @@ void LocalNode::addListener( ConnectionPtr connection )
 {
     LBASSERT( isListening( ));
     LBASSERT( connection->isListening( ));
+    if( !isListening() || connection->isListening( ))
+        return;
 
-    connection->ref( this );
+    connection->ref( this ); // unref in self handler
 
     // Update everybody's description list of me, add the listener to myself in
     // my handler

co/node.cpp
@@ -164,6 +164,7 @@ ConnectionPtr Node::useMulticast()
 
 void Node::addConnectionDescription( ConnectionDescriptionPtr cd )
 {
+    LBASSERTINFO( isClosed(), *this );
     if( cd->type >= CONNECTIONTYPE_MULTICAST && cd->port == 0 )
         cd->port = EQ_DEFAULT_PORT;
 
@@ -173,6 +174,8 @@ void Node::addConnectionDescription( ConnectionDescriptionPtr cd )
 
 bool Node::removeConnectionDescription( ConnectionDescriptionPtr cd )
 {
+    LBASSERTINFO( isClosed(), *this );
+
     lunchbox::ScopedFastWrite mutex( _impl->connectionDescriptions );
 
     // Don't use std::find, RefPtr::operator== compares pointers, not values.

commit d43c3c4c1cff108eb6b4703a88ed9ae05ba3b66b

Fix errornous assertion introduced in e8aa2c7309fd93d85955ed68023c8fd416b69c49

co/localNode.cpp
@@ -1903,7 +1903,7 @@ bool LocalNode::_cmdAddListener( ICommand& command )
     std::string data = command.get< std::string >();
 
     ConnectionDescriptionPtr description = new ConnectionDescription( data );
-    command.getNode()->addConnectionDescription( description );
+    command.getNode()->_addConnectionDescription( description );
 
     if( command.getNode() != this )
         return true;
@@ -1929,7 +1929,7 @@ bool LocalNode::_cmdRemoveListener( ICommand& command )
     std::string data = command.get< std::string >();
 
     ConnectionDescriptionPtr description = new ConnectionDescription( data );
-    LBCHECK( command.getNode()->removeConnectionDescription( description ));
+    LBCHECK( command.getNode()->_removeConnectionDescription( description ));
 
     if( command.getNode() != this )
         return true;

co/node.cpp
@@ -165,6 +165,11 @@ ConnectionPtr Node::useMulticast()
 void Node::addConnectionDescription( ConnectionDescriptionPtr cd )
 {
     LBASSERTINFO( isClosed(), *this );
+    _addConnectionDescription( cd );
+}
+
+void Node::_addConnectionDescription( ConnectionDescriptionPtr cd )
+{
     if( cd->type >= CONNECTIONTYPE_MULTICAST && cd->port == 0 )
         cd->port = EQ_DEFAULT_PORT;
 
@@ -175,7 +180,11 @@ void Node::addConnectionDescription( ConnectionDescriptionPtr cd )
 bool Node::removeConnectionDescription( ConnectionDescriptionPtr cd )
 {
     LBASSERTINFO( isClosed(), *this );
+    return _removeConnectionDescription( cd );
+}
 
+bool Node::_removeConnectionDescription( ConnectionDescriptionPtr cd )
+{
     lunchbox::ScopedFastWrite mutex( _impl->connectionDescriptions );
 
     // Don't use std::find, RefPtr::operator== compares pointers, not values.

co/node.h
@@ -144,6 +144,11 @@ namespace detail { class Node; }
         /** Destructs this node. */
         CO_API virtual ~Node();
 
+        /** @internal */
+        void _addConnectionDescription( ConnectionDescriptionPtr cd );
+        /** @internal */
+        bool _removeConnectionDescription( ConnectionDescriptionPtr cd );
+
     private:
         detail::Node* const _impl;
         CO_API friend std::ostream& operator << ( std::ostream&, const Node& );