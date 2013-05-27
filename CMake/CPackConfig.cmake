c0400aa0c588e151b5ac430c98ad6a7f4182f723
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-05-27 10:22:55 +0200

    Tweak dependencies

--- CMake/CPackConfig.cmake
+++ CMake/CPackConfig.cmake
@@ -9,9 +9,9 @@ set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Object-Oriented C++ Network Library")
 set(CPACK_PACKAGE_DESCRIPTION_FILE ${CMAKE_SOURCE_DIR}/doc/RelNotes.md)
 set(CPACK_RESOURCE_FILE_README ${CMAKE_SOURCE_DIR}/doc/RelNotes.md)
 
-set(CPACK_DEBIAN_BUILD_DEPENDS ${${UPPER_PROJECT_NAME}_BUILD_DEBS}
-  ${LUNCHBOX_DEB_DEPENDENCIES})
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "libstdc++6, libboost-system-dev, libboost-date-time-dev, libboost-regex-dev, libboost-serialization-dev, librdmacm-dev, libibverbs-dev, librdmacm-dev, libudt-dev, ${LUNCHBOX_DEB_DEPENDENCIES}")
+set(CPACK_DEBIAN_BUILD_DEPENDS
+  ${COLLAGE_BUILD_DEBS} ${LUNCHBOX_DEB_DEV_DEPENDENCY})
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libstdc++6, libboost-system-dev, libboost-date-time-dev, libboost-regex-dev, libboost-serialization-dev, librdmacm-dev, libibverbs-dev, librdmacm-dev, libudt-dev, ${LUNCHBOX_DEB_DEV_DEPENDENCY}")
 
 set(CPACK_MACPORTS_CATEGORY devel)
 set(CPACK_MACPORTS_DEPENDS boost Lunchbox)
