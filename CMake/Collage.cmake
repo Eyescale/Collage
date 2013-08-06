9f406f1b746b8c7aa8854a51ecb4746b29d53d3d
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-08-06 15:36:53 +0200

    add files needed for self-contained Buildyard bootstrap

---
+++ CMake/Collage.cmake
@@ -0,0 +1,15 @@
+
+
+set(COLLAGE_PACKAGE_VERSION 1.0.0)
+set(COLLAGE_DEPENDS OFED UDT REQUIRED Boost Lunchbox)
+set(COLLAGE_DEB_DEPENDS librdmacm-dev libibverbs-dev librdmacm-dev libudt-dev
+  libboost-date-time-dev libboost-regex-dev libboost-serialization-dev
+  libboost-system-dev)
+set(COLLAGE_BOOST_COMPONENTS "system regex date_time serialization")
+set(COLLAGE_REPO_URL https://github.com/Eyescale/Collage.git)
+set(COLLAGE_REPO_TAG master)
+set(COLLAGE_FORCE_BUILD ${CI_BUILD})
+
+set(COLLAGE_REPO_TAG HEAD)
+set(COLLAGE_FORCE_BUILD ON)
+set(COLLAGE_SOURCE ${CMAKE_SOURCE_DIR})
