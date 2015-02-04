9fad6b3f298ce5374fce8de4478268b908dac5f9
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2015-02-04 09:25:43 +0100

    Fix build

--- co/dataOStream.h
+++ co/dataOStream.h
@@ -1,5 +1,5 @@
 
-/* Copyright (c) 2007-2014, Stefan Eilemann <eile@equalizergraphics.com>
+/* Copyright (c) 2007-2015, Stefan Eilemann <eile@equalizergraphics.com>
  *                    2010, Cedric Stalder <cedric.stalder@gmail.com>
  *               2012-2014, Daniel Nachbaur <danielnachbaur@gmail.com>
  *
@@ -29,6 +29,7 @@
 #include <lunchbox/nonCopyable.h> // base class
 #include <lunchbox/stdExt.h>
 
+#include <boost/noncopyable.hpp>
 #include <boost/type_traits.hpp>
 #include <map>
 #include <set>
