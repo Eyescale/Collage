f550cf45ff5f4e99f733c37f7b80c8ece9d9fabe
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2015-07-08 11:07:45 +0200

    Minor cleanup

--- co/localNode.cpp
+++ co/localNode.cpp
@@ -273,13 +273,6 @@ LocalNode::~LocalNode( )
 
 bool LocalNode::initLocal( const int argc, char** argv )
 {
-#ifndef NDEBUG
-    LBVERB << lunchbox::disableFlush << "args: ";
-    for( int i=0; i<argc; i++ )
-         LBVERB << argv[i] << ", ";
-    LBVERB << std::endl << lunchbox::enableFlush;
-#endif
-
     // We do not use getopt_long because it really does not work due to the
     // following aspects:
     // - reordering of arguments
